d311fc1b1a2d91199ab8b1b72953ac0b19755a96
Jonas <jappels@users.noreply.github.com>
2020-02-18 17:46:24 +0100

    Update Centroid.py
    The centroid calculation code now imports the stiffener centroid positions from the stiffener_spacing code

--- Centroid.py
+++ Centroid.py
@@ -1,5 +1,7 @@
 import numpy as np
 import math as m
+from Stiffener_Spacing import *
+
 
 ######################## Part I - parameters as in assignment #######################################
 aircraft = "Do228" # Write either A320, F100, CRJ700 or Do228 (bear in mind capitals); this is used for aerodynamic loading
@@ -22,8 +24,8 @@ theta = m.radians(25)  # rad
 P = 20.6*1000  # N
 
 
-z1 = np.linspace(-0.391, 0.124, nst)
-z2 = np.linspace(-0.124, 0.124, nst)
+z = diststiff()[2]
+y = diststiff()[3]
 
 
 
@@ -39,16 +41,19 @@ def z_centroids():      #### gives an array with 15 positions. it stores the z-c
     z_bar_hc = ha / m.pi
     z_bar_sk = -((Ca - ha / 2) / 2)
     z_bar_sp = 0
-    a = z1
+    a = z
     return np.append(a, [z_bar_sp, z_bar_hc, z_bar_sk, z_bar_sk])
 
 def y_centroids():      #### gives an array with 15 positions. it stores the y-coordinates of the centroid of each component. Pos. 0-10 stringer, pos. 11 spar, pos.12 semi circle, pos.13+14 skin
-    a = z2
+    a = y
     return np.append(a, [0, 0, 0.25*ha, -0.25*ha])
 
 def get_total_z_centroid():     #### gives z-coordinate of centroid of whole strucutre
     return sum(z_centroids()*areas()) / sum(areas())
 
+def get_total_y_centroid():     #### gives y-coordinate of centroid of whole strucutre
+    #return sum(y_centroids()*areas()) / sum(areas())
+    return 0
 
 #offset_centroid_crosspoint_stringer = (hst/2)*hst*tst/areas()[0]
 
@@ -58,3 +63,9 @@ print(y_centroids())
 #print(z_centroids())
 
 #print(get_total_z_centroid())
+
+print(diststiff()[2])
+print(diststiff()[3])
+
+print(get_total_z_centroid())
+print(get_total_y_centroid())
