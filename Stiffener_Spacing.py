190c885155f76301b8e88d64ac66baffda95b234
Sebastian2597 <Sebastian2597@users.noreply.github.com>
2020-02-19 09:33:38 +0100

    Update Stiffener_Spacing.py
    Made it a bit nicer and added more functions

--- Stiffener_Spacing.py
+++ Stiffener_Spacing.py
@@ -1,21 +1,11 @@
-from math import *
 from numpy import *
 import matplotlib.pyplot as plt
 
 
 # Python program to calculate the spacing of the stiffeners including its width
 
-def diststiff():
-
-    C_a = 0.515 # C_a Chord length aileron m    (Dornier Do 228: 0.515 m)
-    h_a = 24.8  # h_a Aileron height cm         (Dornier Do 228: 24.8 cm) 
-    n_st = 11   # n_st Number of stiffners      (Dornier Do 228: 11)
-
-    # Converting all lengths to meters
-
-    h_a = h_a/100 # h_a Aileron height m 
-    r = h_a/2
-
+def diststiff(C_a, h_a, n_st):
+    
     # Calculation of the perimeter of the aileron
 
     Circ = pi*h_a/2                      # Calculation of the circumference of the semi-circle
@@ -30,16 +20,18 @@ def diststiff():
 
     d_st = Peri/n_st
 
-    # Determining Y and Z positions of each stiffener
+    return d_st
 
+def stiffcoord(d_st, r, C_a):
+    
+    # Creating 11x1 array of zeroes for the Y-axis and Z-axis for the stiffeners
     Zlst = zeros((11))    
-    Ylst = zeros((11))
-    Zcent = zeros((11))
-    Ycent = zeros((11))
+    Ylst = zeros((11))    
 
-    theta = d_st/r
     Zlst[0] = r
     Ylst[0] = 0
+
+    theta = d_st/r
 
     Zlst[1] = r*cos(theta)
     Ylst[1] = r*sin(theta)
@@ -70,6 +62,23 @@ def diststiff():
         Zlst[j] = Zlst[j-1] + d_st*cos(delta)
         Ylst[j] = (Ylst[j-1] - d_st*sin(delta))
 
+        ##    for i in range(len(Zlst)):
+    ##        print("(",Zlst[i],",", Ylst[i],")", "\t i =", i)
+
+
+    ##zline = [-r,0.391]
+    ##yline = [0,0]
+    ##plt.plot(-Zlst, -Ylst, 'o')
+    ##plt.plot(zline, yline)
+    ##plt.show()
+        
+    return Zlst, Ylst
+
+def stiffcentcoord(Zlst, Ylst):
+
+    Zcent = zeros((11))
+    Ycent = zeros((11))
+
     Zcent[0] = Zlst[0]-2.5/1000
     Ycent[0] = Ylst[0]
 
@@ -85,21 +94,15 @@ def diststiff():
 
     for i in range(6,10):
         Zcent[i] = Zlst[i]+0.76/1000
-        Ycent[i] = Ylst[i]+2.38/1000   
+        Ycent[i] = Ylst[i]+2.38/1000
 
-##    for i in range(len(Zlst)):
-##        print("(",Zlst[i],",", Ylst[i],")", "\t i =", i)
+    return Zcent, Ycent
+
+
     
 
-    ##zline = [-r,0.391]
-    ##yline = [0,0]
-    ##plt.plot(-Zlst, -Ylst, 'o')
-    ##plt.plot(zline, yline)
-    ##plt.show()
-        
-    return Zlst, Ylst, Zcent, Ycent
 
-def Arc(r, theta):
+def Arc(r, gamma):
 	""" Function to compute the y and z coordinates of the LE arc 
 	Input Arguments:
 		r = radius of the arc (float) [m]
@@ -107,12 +110,12 @@ def Arc(r, theta):
 	Output:
 		y, z coordinates
 	"""
-	y = r*sin(theta);
-	z = r*cos(theta);
+	y = r*sin(gamma);
+	z = r*cos(gamma);
 	return z, y;
 
-def Triangel(height, length, z):
-	""" Function to compute the y and z coordinates of the trangel shape 
+def Triangle(height, length, z):
+	""" Function to compute the y and z coordinates of the triangle shape 
 	Input Arguments:
 		h = Height of the triangle (float) [m]
 		length = Length of the airfoil - radius of arc (float) [m]
@@ -124,16 +127,24 @@ def Triangel(height, length, z):
 	y1 = -height - dydz*z;
 	return y, y1;
 
-Zlst,Ylst,Zcent,Ycent = diststiff()
+
 
 N = 10
-h = 0.248
 t = 1.1e-3
-length = 0.515
-theta = linspace(0, pi/2, N)
-z_cord = linspace(0, length - h/2, N)
-z, y = Arc(h/2, theta)
-y_, y_1 = Triangel(h/2, length - h/2, z_cord)
+C_a = 0.515     # C_a Chord length aileron m    (Dornier Do 228: 0.515 m)
+h_a = 24.8/100  # h_a Aileron height m         (Dornier Do 228: 24.8 cm) 
+n_st = 11       # n_st Number of stiffners      (Dornier Do 228: 11)
+r = h_a/2
+gamma = linspace(0, pi/2, N)
+z_cord = linspace(0, C_a - h_a/2, N)
+
+
+z, y = Arc(h_a/2, gamma)
+y_, y_1 = Triangle(h_a/2, C_a - h_a/2, z_cord)
+
+d_st = diststiff(C_a, h_a, n_st)
+Zlst, Ylst = stiffcoord(d_st, r, C_a)
+Zcent, Ycent = stiffcentcoord(Zlst, Ylst)
 
 zline = [-0.124,0.391]
 yline = [0,0]
@@ -145,8 +156,8 @@ plt.plot(-z, -y, color='blue')
 plt.plot(z_cord, y_, color='blue')
 plt.plot(z_cord, y_1, color='blue')
 plt.plot(-Zlst, -Ylst, 'o', color='red')
-plt.plot(zline, yline, color='black')
-plt.plot(zspar,yspar, color='black', linestyle='dashed')
+plt.plot(zline, yline, color='black', linestyle='dashed')
+plt.plot(zspar,yspar, color='black')
 plt.plot(-Zcent, -Ycent,"o" ,color="purple")
 plt.grid(True)
 plt.show()
