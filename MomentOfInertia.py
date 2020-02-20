f3ebaab54e1ada2a80db118ffa284ba379fde374
MartijnKanger <MartijnKanger@users.noreply.github.com>
2020-02-20 15:18:04 +0100

    Update MomentOfInertia.py

--- MomentOfInertia.py
+++ MomentOfInertia.py
@@ -47,9 +47,9 @@ L = np.pi*R + 2* l
 d = L/11
 circ = 2*np.pi*R
 angle = 360 * d/circ
-
+stiffangle = 90 - angle
 #print(stiffangle)
-theta2 = m.radians(angle)
+theta2 = m.radians(stiffangle)
 theta3 = m.radians(90)
 z = R*m.cos(m.radians(angle))
 y = R*m.sin(m.radians(angle))
@@ -95,7 +95,7 @@ def Aspar(h,t):
     #input the height and thickness of the spar. Output the area of the spar.
     return (h*t)
 
-def Alstgen(Nstringer,Nspar,Nseml,Npanel):
+def Alstgen(Nstringer,Nspar,Npanel,Nsemi):
     #input the amount of each element, output a list of all the areas.
     Alst = []
     for i in range(Nstringer):
@@ -109,9 +109,7 @@ def Alstgen(Nstringer,Nspar,Nseml,Npanel):
 
     return Alst
 
-
-
-Alst = Alstgen(Nstringer,Nspar,Nsemi,Npanel)
+Alst = Alstgen(Nstringer,Nspar,Npanel,Nsemi)
 #print(Alst)
 
 def MoIrect(b,h):
@@ -136,6 +134,7 @@ def MoIstringery(tst,hst,wst):
     #input the dimensions of the stringer, output the MoIyy about its centroid.
     return(MoIrect(hst,tst) + MoIrect(tst,wst))
 
+<<<<<<< HEAD
 def MoIrotrec(b,h,theta):
     return ((b * h)*(b**2 * m.cos(m.radians(90)-theta)**2 + h**2 * m.sin(m.radians(90)-theta)**2)/12)
 
@@ -146,6 +145,8 @@ print('test',MoIrotrec(l,tsk,-theta1))
 print(MoIrotrec2(l,tsk,-theta1))
 
 
+=======
+>>>>>>> 4e4da82c3eaecb0ec9913bc7fb070dbc432a97c0
 def MoIconv(Ixx,Iyy,theta):
     "This program calculates the Moment of Inertia when rotating the reference frame."
     "Input the moment of inertia about axis system x-y, and the rotation angle theta."
@@ -154,25 +155,19 @@ def MoIconv(Ixx,Iyy,theta):
     Ivv = (Ixx+Iyy)/2 - m.cos(2*theta)*(Ixx-Iyy)/2
     return Iuu,Ivv
 
-def MoIlstgen(Nstringer,Nspar,Nsemi,Npanel,theta1,theta2,theta3):
+def MoIlstgen(Nstringer,Nspar,Npanel,Nsemi,theta1,theta2,theta3):
     #input the amount of elements and an angle theta, output a list of the MoIxx and MoIyy of the different elements.
     MoIxlst = []
     MoIylst = []
     for i in range(Nstringer-3):
-        MoIxlst += [0]
-        MoIylst += [0]
-        #MoIxlst += [MoIconv(MoIstringerx(tst,hst,wst),MoIstringery(tst,hst,wst),theta1)[0]]
-        #MoIylst += [MoIconv(MoIstringerx(tst,hst,wst),MoIstringery(tst,hst,wst),theta1)[1]]
+        MoIxlst += [MoIconv(MoIstringerx(tst,hst,wst),MoIstringery(tst,hst,wst),theta1)[0]]
+        MoIylst += [MoIconv(MoIstringerx(tst,hst,wst),MoIstringery(tst,hst,wst),theta1)[1]]
     for i in range(2):
-        MoIxlst += [0]
-        MoIylst += [0]
-        #MoIxlst += [MoIconv(MoIstringerx(tst,hst,wst),MoIstringery(tst,hst,wst),theta2)[0]]
-        #MoIylst += [MoIconv(MoIstringerx(tst,hst,wst),MoIstringery(tst,hst,wst),theta2)[1]]
+        MoIxlst += [MoIconv(MoIstringerx(tst,hst,wst),MoIstringery(tst,hst,wst),theta2)[0]]
+        MoIylst += [MoIconv(MoIstringerx(tst,hst,wst),MoIstringery(tst,hst,wst),theta2)[1]]
     for i in range(1):
-        MoIxlst += [0]
-        MoIylst += [0]
-        #MoIxlst += [MoIconv(MoIstringerx(tst,hst,wst),MoIstringery(tst,hst,wst),theta3)[0]]
-        #MoIylst += [MoIconv(MoIstringerx(tst,hst,wst),MoIstringery(tst,hst,wst),theta3)[1]]        
+        MoIxlst += [MoIconv(MoIstringerx(tst,hst,wst),MoIstringery(tst,hst,wst),theta3)[0]]
+        MoIylst += [MoIconv(MoIstringerx(tst,hst,wst),MoIstringery(tst,hst,wst),theta3)[1]]        
     for i in range(Nspar):
         MoIxlst += [MoIrect(tsp,h)] 
         MoIylst += [MoIrect(h,tsp)]
@@ -180,59 +175,16 @@ def MoIlstgen(Nstringer,Nspar,Nsemi,Npanel,theta1,theta2,theta3):
         MoIxlst += [MoIsemi(R,r)]  
         MoIylst += [MoIsemi(R,r)]
     for i in range(Npanel):
-        MoIxlst += [MoIrotrec(l,tsk,theta)]
-        MoIylst += [MoIrotrec(tsk,l,m.radians(90)-theta)]
-        #MoIxlst += [MoIconv(MoIrect(l,tsk),MoIrect(tsk,l),theta1)[0]]
-        #MoIylst += [MoIconv(MoIrect(l,tsk),MoIrect(tsk,l),theta1)[1]]
+        MoIxlst += [MoIconv(MoIrect(l,tsk),MoIrect(tsk,l),theta)[0]]
+        MoIylst += [MoIconv(MoIrect(l,tsk),MoIrect(tsk,l),theta)[1]]
 
     return MoIxlst, MoIylst
 
-MoIxlst,MoIylst = MoIlstgen(Nstringer,Nspar,Nsemi, Npanel,theta,theta2,theta3)
-print(MoIxlst)
-print(MoIconv(MoIrect(l,tsk),MoIrect(tsk,l),theta1)[0])
+MoIxlst,MoIylst = MoIlstgen(Nstringer,Nspar,Npanel,Nsemi,theta,0,0)
+print(MoIxlst,MoIylst)
 
-
-(zc,yc) = (-0.08327784945088734,0)
-
-#print(z)
-#print(y)
-# order is: stringers, spar, semi, top, bottom.
-
-zcentroidlst = [ 0.1215,      0.07675071 ,-0.0232709 , -0.12811921 ,-0.23296753, -0.33781584,
- -0.33781584, -0.23296753 ,-0.12811921 ,-0.0232709  , 0.07675071 , 0.,
-  0., -0.1955     ,-0.1955    ]
-
-ycentroidlst = [ 0.         , 0.09418648  ,0.11399895 , 0.08074782 , 0.04749669  ,0.01424556,
- -0.01424556 ,-0.04749669 ,-0.08074782 ,-0.11399895, -0.09418648  ,0.,
-  0.,0.062 ,     -0.062     ]
-
+[z,y] = ()
 def Steinerlstgen():
-    Alst = Alstgen(Nstringer,Nspar,Nsemi,Npanel)
     Steinerxlst = []
     Steinerylst = []
-    for i in range(Nstringer+Nspar+Nsemi+Npanel):
-        #print(abs(z-zcentroidlst[i]))
-        Steinerxlst += [Alst[i]*(ycentroidlst[i])**2]
-        Steinerylst += [Alst[i]*(abs(zc-zcentroidlst[i]))**2]
-    return Steinerxlst, Steinerylst
-
-Steinerxlst,Steinerylst = Steinerlstgen()
-
-#print(Steinerxlst,Steinerylst)
-
-MoIzz = sum(MoIxlst) + sum(Steinerxlst)
-MoIyy = sum(MoIylst) + sum(Steinerylst)
-
-
-print("Izz = ",MoIzz)
-print("Iyy = ",MoIyy)
-
-#Izz = 1.028e-05
-#Iyy = 8.651e-05
-
-print(sum(Steinerxlst))
-print(sum(Steinerylst))
-
-testMoIxx = sum(Steinerxlst) + MoIrect(tsp,h) + MoIsemi(R,r) + 2*MoIrect(l,tsk)
-print(testMoIxx)
 
